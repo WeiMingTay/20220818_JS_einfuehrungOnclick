f89225709dfce077f0994f877dc782b45c628c4c
WeiMingTay <110397919+WeiMingTay@users.noreply.github.com>
2022-08-18 20:46:48 +0200

    version01

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function changebg1() {
+    bgMain.style.backgroundColor="#1D8F53"
+    bgH1.style.color="#6E0860"
+}
+function changebg2() {
+    bgMain.style.backgroundColor="#444170"
+    bgH1.style.color="#D9AB41"
+}
+function changebg3() {
+    bgMain.style.backgroundColor="#00001F"
+    bgH1.style.color="#C65E0A"
+}
+function changebg4() {
+    bgMain.style.backgroundColor="#040352"
+    bgH1.style.color="#29CCFE"
+}
